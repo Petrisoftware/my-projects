627747a2f06faf0a8a33820bd09bf8d2be194f04
Petri Kemppinen <petri.j.kemppinen@gmail.com>
2019-01-30 10:45:27 +0200

    Defined the rods

---
+++ hanoi/hanoi.py
@@ -0,0 +1,19 @@
+# Tower of Hanoi
+# Petri Kemppinen 2019
+
+# This is a Python implementation of the game Tower of Hanoi.
+
+
+def main():
+    # Welcome
+    print("Welcome to Tower of Hanoi")
+
+    # Creating the three rods
+    rod1 = []
+    rod2 = []
+    rod3 = []
+
+    return
+
+
+main()
