3e7c1756d9d8796e5071bb9a4d7c7cdc03d40b4f
Petri Kemppinen <petri.j.kemppinen@gmail.com>
2019-02-05 20:12:26 +0200

    Seed random generator

--- subgenerator/subgenerator.py
+++ subgenerator/subgenerator.py
@@ -2,4 +2,25 @@
 # Subigeneraattori
 
 # Generoi käyttäjälle satunnaisen Subway-subin.
-# 
+# Valitsee yhden leivän, yhden täytteen ja satunnaisen määrän kasviksia ja
+# kastikkeita.
+
+import random
+
+
+def main():
+    # Tervetuloa.
+    print("Welcome to Subway Generator 2019")
+
+    # Luetaan ainesosat tiedostoista.
+
+    # Seed satunnaisgeneraattorille.
+    seed = input("Enter value for random generator: ")
+    random.seed(seed)
+
+    # TODO: arvonta
+
+    return
+
+
+main()
