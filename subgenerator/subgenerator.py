146557a3fe7ae9f25f18642b20a307fc8727f8d5
Petri Kemppinen <petri.j.kemppinen@gmail.com>
2019-02-05 20:28:18 +0200

    Aloitettu tiedostonlukufunktion kirjoittaminen

--- subgenerator/subgenerator.py
+++ subgenerator/subgenerator.py
@@ -7,12 +7,40 @@
 
 import random
 
+# Tiedostot, jotka ohjelman täytyy lukea.
+FILES = ["leipa.txt", "tayte.txt", "kasvis.txt", "kastike.txt"]
+
+
+# Lukee ainesosat tiedostoista ja tallettaa ne listaan.
+def readfiles(lista, aines):
+    # Lue lista riippuen siitä, mikä on kyseessä.
+    if aines == "leipa":
+        try:
+            f = open("leipa.txt", 'r')
+            f.close()
+        except OSError:
+            print("Error! File leipa.txt not found!")
+    elif aines == "tayte":
+        # eeee
+    elif aines == "kasvis":
+        # aaa
+    elif aines == "kastike":
+        # uliuliuli
+    else:
+        print("This shouldn't happen.")
+
+    return lista
+
 
 def main():
     # Tervetuloa.
     print("Welcome to Subway Generator 2019")
 
     # Luetaan ainesosat tiedostoista.
+    leipa = []
+    tayte = []
+    kasvis = []
+    kastike = []
 
     # Seed satunnaisgeneraattorille.
     seed = input("Enter value for random generator: ")
